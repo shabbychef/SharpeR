52079a2cb2230ecdea6ead3539bfec52dbccf4ea
Steven Pav <shabbychef@gmail.com>
2014-12-17 22:16:46 -0800

    problems loading this via curl.
    so just get it via makefile or git.

--- nodist/make_gmacro_data.R
+++ nodist/make_gmacro_data.R
@@ -30,35 +30,38 @@ read.https.csv <- function(url,...) {
 	return(retval)
 }
 
-shiller.data <- read.https.csv("https://raw.github.com/datasets/s-and-p-500/master/data/data.csv",
+#curl -o data.csv 'https://raw.github.com/datasets/s-and-p-500/master/data/data.csv'
+#shiller.data <- read.https.csv("https://raw.github.com/datasets/s-and-p-500/master/data/data.csv",
+															 #stringsAsFactors=FALSE)
+
+shiller.data <- read.csv("sp500_data.csv",
 															 stringsAsFactors=FALSE)
 shill.xts <- xts(shiller.data[,!colnames(shiller.data) %in% c("Date")],
 								 order.by=alignMonthly(as.Date(shiller.data$Date),include.weekends=TRUE))
 
 
-# 
-# load data from quandl.
-get.quandl <- function(urlend,date.col='Date',colClasses=NULL,...) {
-	furl <- paste('http://www.quandl.com/api/v1/datasets/',
-								urlend,sep="")
+require(Quandl)
 
-	if (is.null(colClasses)) {
-		colClasses <- c('Date')
-		names(colClasses) <- date.col
-	}
-
-	df.dat <- read.csv(furl, colClasses=colClasses, ...)
-	df.xts <- xts(df.dat[,!colnames(df.dat) %in% date.col],
-								order.by=df.dat[,date.col])
-	return(df.xts)
+# Quandl
+if (require(utils) && require(Quandl)) {
+	quandl.auth <- Sys.getenv('QUANDL_AUTH')
+	options(quandl.auth = ifelse(nchar(quandl.auth),quandl.auth,""))
+	rm(quandl.auth)
+	Quandl.auth(options()$quandl.auth)
 }
 
-ff10.xts <- get.quandl('KFRENCH/10_IND_PORTF_M.csv?&auth_token=EsppzNgJHtEGaH2gqepZ&trim_start=1926-07-31&trim_end=2012-12-31&sort_order=desc')
-ff5.xts <- get.quandl('KFRENCH/5_IND_PORTF_M.csv?&auth_token=EsppzNgJHtEGaH2gqepZ&trim_start=1926-07-31&trim_end=2012-12-31&sort_order=desc')
-fff.xts <- get.quandl('KFRENCH/FACTORS_M.csv?&auth_token=EsppzNgJHtEGaH2gqepZ&trim_start=1926-07-31&trim_end=2013-10-31&sort_order=desc', date.col='Month')
-
-dp.xts <- get.quandl('YALE/SP_PER10Y.csv?&auth_token=EsppzNgJHtEGaH2gqepZ&trim_start=1881-12-31&trim_end=2013-12-31&sort_order=desc', date.col='Year')
-
+dp.xts <- Quandl("YALE/SP_PER10Y",
+	start_date="1881-12-31",end_date="2013-12-31",
+	type="xts")
+ff10.xts <- Quandl("KFRENCH/10_IND_PORTF_M",
+	start_date="1926-07-31",end_date="2012-12-31",
+	type="xts")
+ff5.xts <- Quandl("KFRENCH/5_IND_PORTF_M",
+	start_date="1926-07-31",end_date="2012-12-31",
+	type="xts")
+fff.xts <- Quandl("KFRENCH/FACTORS_M",
+	start_date="1926-07-31",end_date="2013-10-31",
+	type="xts")
 
 # o.... 2FIX
 save(ff5.xts,ff10.xts,fff.xts,dp.xts,shill.xts,file='gmacro_data.rda',compress='bzip2')
