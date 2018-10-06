40e048bb89166b0d00bfff16ecc31588bb17a51f
Steven Pav <shabbychef@gmail.com>
2018-10-06 11:47:04 -0700

    adding to help for sr_bias

--- R/sr_bias.r
+++ R/sr_bias.r
@@ -137,8 +137,20 @@
 #' @examples 
 #'
 #' # bias under normality:
-#' sr_bias(1, 100, rep(0,5), type='simple')
+#' sr_bias(1, 100, rep(0,2), type='simple')
 #' sr_bias(1, 100, rep(0,5), type='second_order')
+#'
+#' # plugging in sample estimates
+#' x <- rnorm(1000)
+#' n <- length(x)
+#' mu <- mean(x)
+#' sdv <- sd(x)
+#' snr <- mu / sdv
+#' # these are not great estimates, but close enough:
+#' sku <- mean((x-mu)^3) / sdv^3
+#' kur <- (mean((x-mu)^4) / sdv^4) - 4
+#' sr_bias(snr, n, c(sku,kur), type='simple')
+#'
 #' @export
 sr_bias <- function(snr, n, cumulants, type=c('simple','second_order')) {
 	type <- match.arg(type)
